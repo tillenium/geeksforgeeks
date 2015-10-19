7a1dedcff2fea464f291a597f9f65b47711862b7
Anubhav <Anubhav@apples-MacBook-Pro.local>
2015-10-19 15:48:31 +0530

    added title to csv

--- lib/geeksforgeeks/output.rb
+++ lib/geeksforgeeks/output.rb
@@ -2,6 +2,7 @@ class Geeksforgeeks::Output
 
   def self.create_csv(links,file_name)
     CSV.open("/tmp/#{file_name}","wb") do |csv|
+      csv << ["Questions","Links"]
       links.each do |link|
         csv << link
       end
