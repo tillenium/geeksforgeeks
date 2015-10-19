0722cb62f04a2b11c6937c92c2fa15f504233b73
Anubhav <Anubhav@apples-MacBook-Pro.local>
2015-10-19 15:43:43 +0530

    questions excel

--- lib/geeksforgeeks/problems_downloader.rb
+++ lib/geeksforgeeks/problems_downloader.rb
@@ -1,6 +1,6 @@
 class Geeksforgeeks::ProblemsDownloader
   
-  def self.download(url = "http://www.geeksforgeeks.org/data-structures/", start_link = "Introduction to Linked List", end_link = "Expression Tree",file_name = "DataStructures.csv.")
+  def self.download(url = "http://www.geeksforgeeks.org/data-structures/", start_link = "Introduction to Linked List", end_link = "Expression Tree",file_name = "DataStructures.csv")
     fetcher = Geeksforgeeks::Fetcher.new(url)
     doc = fetcher.fetch_url
     links = Geeksforgeeks::LinkParser.link_to_array doc, start_link, end_link
